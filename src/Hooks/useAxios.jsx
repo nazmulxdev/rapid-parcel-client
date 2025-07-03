0b405aef1d58ce1451ea5e4f1f44e9ab2877a1a1
Md Nazmul Hossen <nazmulhossen.hstu.21@gmail.com>
2025-07-03 16:59:14 +0600

    managed 401 erro

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -1,6 +1,4 @@
 import axios from "axios";
-import React from "react";
-
 const axiosInstance = axios.create({
   baseURL: `http://localhost:3000`,
 });
--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,10 +1,52 @@
 import axios from "axios";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+import useAuth from "./useAuth";
+import useAxios from "./useAxios";
+import Swal from "sweetalert2";
 const axiosSecure = axios.create({
   baseURL: `http://localhost:3000`,
   withCredentials: true,
 });
 
 const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const { logOut } = useAuth();
+  const location = useLocation();
+  const axiosInstance = useAxios();
+  useEffect(() => {
+    const interceptor = axiosSecure.interceptors.response.use(
+      (config) => {
+        return config;
+      },
+      async (error) => {
+        const errorStatus = error?.response?.status;
+        if (errorStatus === 401) {
+          await Swal.fire({
+            icon: "warning",
+            title: "Session expired",
+            text: "Please login again to continue.",
+            confirmButtonText: "OK",
+            confirmButtonColor: "#d33",
+          });
+
+          try {
+            await logOut(); // Firebase logout
+            await axiosInstance.post("/logout", {}, { withCredentials: true }); // Backend logout
+            navigate("/login", {
+              state: location.pathname,
+              replace: true,
+            });
+          } catch (logoutErr) {
+            console.error("Logout failed:", logoutErr);
+          }
+        }
+        // you can handle 403 here
+        return Promise.reject(error);
+      },
+    );
+    return () => axiosSecure.interceptors.response.eject(interceptor);
+  }, [axiosInstance, location, navigate, logOut]);
   return axiosSecure;
 };
 
